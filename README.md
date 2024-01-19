e1bab49513112b6ac5eb29d98bbd6d8236363b0b
yusuf kağan <77469229+yusufkaanklc@users.noreply.github.com>
2024-01-19 20:19:44 +0300

    deneme

---
+++ README.md
@@ -0,0 +1,19 @@
+
+# MongoDB CRUD Example App
+This simple Node.js application serves as a basic CRUD (Create, Read, Update, Delete) operations demonstration using MongoDB as the database.
+
+
+## Installation
+Execute the following command in your terminal to install the project dependencies:
+
+```sh
+npm i mongoose
+```
+
+### Step 4: Launching the Development Server
+
+To start the development server, run:
+
+```sh
+node mongoose-crud-example
+```
---
+++ mongoose-crud-example.js
@@ -0,0 +1,162 @@
+const readline = require("readline");
+const mongoose = require("mongoose");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const Schema = mongoose.Schema;
+
+mongoose.connect("mongodb://127.0.0.1:27017/deneme").then(async () => {
+  console.log("MongoDB Bağlandı");
+  console.log("Merhaba sisteme hoş geldiniz!");
+
+  const askQuestion = (quest, errMessage) => {
+    return new Promise((resolve, reject) => {
+      rl.question(quest, (answer) => {
+        if (answer) resolve(answer);
+        else reject(errMessage);
+      });
+    });
+  };
+
+  const main = async () => {
+    try {
+      const option = (
+        await askQuestion(
+          "Ne yapmak istersiniz? (oku, yaz, güncelle, sil) :",
+          "Seçim işleminde hata oluştu"
+        )
+      ).toLowerCase();
+
+      switch (option) {
+        case "oku":
+          await readData();
+          break;
+        case "yaz":
+          await writeData();
+          break;
+        case "güncelle":
+          await updateData();
+          break;
+        case "sil":
+          await deleteData();
+          break;
+
+        default:
+          //
+          break;
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const dataSchema = new Schema({
+    title: String,
+    description: String,
+    qty: Number,
+  });
+
+  const Data = mongoose.model("data", dataSchema);
+
+  const processRepeat = async () => {
+    const answer = await askQuestion(
+      "Devam etmek istiyormusunuz ? (evet, hayır): ",
+      "Seçim işleminde hata oluştu"
+    );
+    if (answer === "evet") {
+      main();
+    } else {
+      console.log("Programdan çıkıldı");
+      mongoose.connection.close();
+      rl.close();
+    }
+  };
+
+  const readData = async () => {
+    try {
+      const data = await Data.find();
+      console.log(data);
+      processRepeat();
+    } catch (error) {
+      console.log("Veri okunurken hata oluştu: ", error);
+    }
+  };
+
+  const writeData = async () => {
+    try {
+      const askTitle = await askQuestion(
+        "Verinin adını giriniz : ",
+        "Veri ismi girilemedi"
+      );
+
+      const askDescription = await askQuestion(
+        "Verinin tanımını giriniz : ",
+        "Veri tanımı girilemedi"
+      );
+
+      const askQty = await askQuestion(
+        "Verinin miktarını giriniz : ",
+        "Veri miktarı girilemedi"
+      );
+
+      const newData = new Data({
+        title: askTitle,
+        description: askDescription,
+        qty: askQty,
+      });
+
+      await newData.save();
+
+      console.log("Veri eklendi");
+      processRepeat();
+    } catch (error) {
+      console.log("Veri eklerken hata oluştu: ", error);
+    }
+  };
+
+  const updateData = async () => {
+    try {
+      const askTitle = await askQuestion(
+        "Verinin güncellenecek adını giriniz : ",
+        "Veri ismi girilemedi"
+      );
+
+      const getData = await askQuestion(
+        "Verinin güncellenecek tanımını giriniz : ",
+        "Veri tanımı girilemedi"
+      );
+
+      await Data.findOneAndUpdate(
+        { title: askTitle },
+        { $set: { description: getData } },
+        { new: true }
+      );
+
+      console.log("Veri güncellendi");
+      processRepeat();
+    } catch (error) {
+      console.log("Veri güncellenirken hata oluştu: ", error);
+    }
+  };
+
+  const deleteData = async () => {
+    try {
+      const askTitle = await askQuestion(
+        "Silmek istediğiniz Verinin adını giriniz : ",
+        "Veri ismi girilemedi"
+      );
+
+      await Data.findOneAndDelete({ title: askTitle });
+
+      console.log("Veri silindi");
+      processRepeat();
+    } catch (error) {
+      console.log("Veri silinirken hata oluştu: ", error);
+    }
+  };
+
+  main();
+});
